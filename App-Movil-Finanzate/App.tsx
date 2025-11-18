14bc4fb478d18d3778d692d280536fad60dab56e
Daniel Sebastian Angulo <danielangulo@unimayor.edu.co>
2025-11-17 21:14:24 -0500

    Creacion del login

--- App-Movil-Finanzate/App.tsx
+++ App-Movil-Finanzate/App.tsx
@@ -1,20 +1,203 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  SafeAreaView,
+  View,
+  Image,
+  Text,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
 
-export default function App() {
+export default function Home() {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
   return (
-    <View style={styles.container}>
-      <Text>Holaaaa!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#e8ecf4" }}>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Image
+            alt="App Logo"
+            resizeMode="contain"
+            source={require("./assets/logo-finanzate-sin-fondo.png")}
+            style={styles.headerImg}
+          />
+
+          <Text style={styles.title}>
+            Ingresar a <Text style={{ color: "#0253d5ff" }}>Finanzate</Text>
+          </Text>
+
+          <Text style={styles.subtitle}>
+            gestiona tus finanzas, facil y seguro.
+          </Text>
+        </View>
+
+        <View style={styles.form}>
+          <View style={styles.input}>
+            <Text style={styles.inputLabel}>Correo electronico</Text>
+
+            <TextInput
+              autoCapitalize="none"
+              autoCorrect={false}
+              clearButtonMode="while-editing"
+              keyboardType="email-address"
+              onChangeText={(email) => setForm({ ...form, email })}
+              placeholder="example@gmail.com"
+              placeholderTextColor="#6b7280"
+              style={styles.inputControl}
+              value={form.email}
+            />
+          </View>
+
+          <View style={styles.input}>
+            <Text style={styles.inputLabel}>Contraseña</Text>
+
+            <TextInput
+              autoCorrect={false}
+              clearButtonMode="while-editing"
+              onChangeText={(password) => setForm({ ...form, password })}
+              placeholder="********"
+              placeholderTextColor="#6b7280"
+              style={styles.inputControl}
+              secureTextEntry={true}
+              value={form.password}
+            />
+          </View>
+
+          <View style={styles.formAction}>
+            <TouchableOpacity
+              onPress={() => {
+                alert("haz iniciado sesion con exito!");
+              }}
+            >
+              <View style={styles.btn}>
+                <Text style={styles.btnText}>Ingresar</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+
+          <TouchableOpacity
+            onPress={() => {
+              alert(
+                "sigue el link enviado a tu correo para restablecer tu contraseña!"
+              );
+            }}
+          >
+            <Text style={styles.formLink}>Olvidaste tu contraeña?</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <TouchableOpacity
+        onPress={() => {
+          alert("redireccionando a la pagina de registro!");
+        }}
+      >
+        <Text style={styles.formFooter}>
+          No tienes una cuenta?{" "}
+          <Text style={{ textDecorationLine: "underline", color: "#075eec" }}>Creala aqui</Text>
+        </Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 0,
+    padding: 24,
+  },
+
+  title: {
+    fontSize: 31,
+    fontWeight: "700",
+    color: "#1D2A32",
+    marginBottom: 6,
+  },
+  subtitle: {
+    fontSize: 15,
+    fontWeight: "500",
+    color: "#929292",
+  },
+
+  header: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 36,
+  },
+  headerImg: {
+    width: 150,
+    height: 150,
+    alignSelf: "center",
+    marginBottom: 36,
+  },
+
+  form: {
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 0,
+  },
+  formAction: {
+    marginTop: 4,
+    marginBottom: 16,
+  },
+  formLink: {
+    fontSize: 16,
+    fontWeight: "600",
+    color: "#075eec",
+    textAlign: "center",
+  },
+  formFooter: {
+    paddingVertical: 24,
+    fontSize: 15,
+    fontWeight: "600",
+    color: "#222",
+    textAlign: "center",
+    letterSpacing: 0.15,
+  },
+
+  input: {
+    marginBottom: 16,
+  },
+  inputLabel: {
+    fontSize: 17,
+    fontWeight: "600",
+    color: "#222",
+    marginBottom: 8,
+  },
+  inputControl: {
+    height: 50,
+    backgroundColor: "#fff",
+    paddingHorizontal: 16,
+    borderRadius: 12,
+    fontSize: 15,
+    fontWeight: "500",
+    color: "#222",
+    borderWidth: 1,
+    borderColor: "#C9D3DB",
+    borderStyle: "solid",
+  },
+
+  btn: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 30,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderWidth: 1,
+    backgroundColor: "#084cbaff",
+    borderColor: "#075eec",
+  },
+  btnText: {
+    fontSize: 18,
+    lineHeight: 26,
+    fontWeight: "600",
+    color: "#fff",
   },
 });
